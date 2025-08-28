6de44018bf66e08baa8b98cf0a5c6b3b10725098
Szerwigi1410 <szerwigi14@gmail.com>
2025-08-28 12:04:13 +0200

    Added more js scripts

---
+++ brokefetch_js/script7.js
@@ -0,0 +1,14 @@
+function myFunction6() {
+  // Get the text field
+  var copyText = document.getElementById("myInput6");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+} 
